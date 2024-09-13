77711ddccac5f5eab6da8d20ecbcd44e23fdc2c4
Burek-man <Alnu07001@utb.vaxjo.se>
2024-09-13 10:46:01 +0200

    jirgjirgji

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    location.href = "https://www.biltema.se/verktyg/handverktyg/sagar/betongsag-28-2000034776";
+})
